d00848a59789a51e708f43c0e223233010641bb6
Abhishek Kumar <abhishek.nitdelhi@gmail.com>
2024-01-04 17:14:54 +0530

    Radix UI icon removal

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,6 +1,9 @@
 "use client"
 import * as React from "react"
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+
+import { FaMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa6";
+
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
@@ -18,8 +21,8 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <FaSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <FaMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
